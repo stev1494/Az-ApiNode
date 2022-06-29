8944f602dfb2d2dd20a8783ff81b469e292c13bd
stev1494 <stvn.piano@gmail.com>
2022-06-28 23:28:27 -0500

    primero

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({ username: 'Steven'}));
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
